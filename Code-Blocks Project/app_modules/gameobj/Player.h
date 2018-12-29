d308b5f2e3cbc60446ac235cf6b5a7b3e8092461
Is Daouda <45235532+Is-Daouda@users.noreply.github.com>
2018-12-29 15:08:59 +0000

    Add files via upload

---
+++ Code-Blocks Project/app_modules/gameobj/Player.h
@@ -0,0 +1,35 @@
+#ifndef PLAYER_H_INCLUDED
+#define PLAYER_H_INCLUDED
+
+#include "../core/GameSystemExtended.h"
+#include "../system/entity/MainObject.h"
+#include "../system/entity/ObjectActivity.h"
+#include "Bloc.h"
+
+class Player : public is::MainObject, public is::ObjectActivity
+{
+public:
+    Player();
+
+    void loadResources(sf::Texture &m_tex);
+    void step(float const &DELTA_TIME, bool &leftJoystickPressed, bool &rigthJoystickPressed);
+    void draw(sf::RenderWindow &app);
+    void setAngle(float val);
+    void setSpeed(float val);
+    void setHurt(bool val);
+    void setRotationRight(bool val);
+
+    float getAngle()        const;
+    float getSpeed()        const;
+    float getImageAngle()   const;
+    bool getHurt()          const;
+    bool getRotationRight() const;
+
+private:
+    float m_rotation, m_speed, m_angle, m_imageAngle;
+    float m_frame;
+    float const MAX_SPEED;
+    bool m_rotationRight, m_hurt;
+};
+
+#endif // PLAYER_H_INCLUDED
---
+++ Code-Blocks Project/app_modules/level/Level.h
@@ -0,0 +1,8 @@
+#ifndef LEVEL_H_INCLUDED
+#define LEVEL_H_INCLUDED
+
+#include "../level/level_1.h"
+#include "../level/level_2.h"
+#include "../level/level_3.h"
+
+#endif // LEVEL_H_INCLUDED
